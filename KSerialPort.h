ed6c00a2c369b9e1f4b8a28338389d8d90430383
Kiyoshi Irie <kiyoshiiriemon@gmail.com>
2020-03-22 11:34:27 +0900

    boost serial port wrapper

---
+++ KSerialPort.h
@@ -0,0 +1,141 @@
+/****************************************************************************
+ * KSerialPort
+ * Copyright (C) 2012 Kiyoshi Irie
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/.
+ ****************************************************************************/
+
+#ifndef KSERIALPORT_H_
+#define KSERIALPORT_H_
+
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+class KSerialPort
+{
+public:
+	KSerialPort() : mport(io_srv) {
+	}
+
+	typedef enum {
+		KS_PARITY_NONE,
+		KS_PARITY_EVEN,
+		KS_PARITY_ODD
+	} ParityType;
+
+	boost::system::error_code open(const std::string portname) {
+		boost::system::error_code err;
+		if (!mport.open(portname, err)) {
+			mport.set_option(boost::asio::serial_port_base::character_size(8));
+			mport.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
+			mport.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
+			mport.set_option(boost::asio::serial_port_base::baud_rate(115200));
+			mport.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
+		}
+		return err;
+	}
+	void close() {
+		mport.close();
+	}
+	void setBaudRate(int rate) {
+		mport.set_option(boost::asio::serial_port_base::baud_rate(rate));
+	}
+	void setParity(ParityType p) {
+		switch(p) {
+		case KS_PARITY_NONE:
+			mport.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
+			break;
+		case KS_PARITY_EVEN:
+			mport.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::even));
+			break;
+		case KS_PARITY_ODD:
+			mport.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::odd));
+			break;
+		}
+	}
+#if 0
+	void set_option(const SettableSerialPortOption& option) {
+		mport.set_option(option);
+	}
+#endif
+	int read_some(std::vector<uint8_t> &buffers, boost::system::error_code& ec) {
+		return mport.read_some(boost::asio::buffer(buffers, buffers.size()), ec);
+	}
+	int read_some(uint8_t *buffers, int len, boost::system::error_code& ec) {
+		return mport.read_some(boost::asio::buffer(buffers, len), ec);
+	}
+
+	void timeout_callback(boost::asio::serial_port& port, const boost::system::error_code& error) {
+		if (!error) {
+			//timeout
+			port.cancel();
+			//std::cout << "serial read timeout" << std::endl;
+		}
+	}
+	int read(uint8_t *buffers, int len, boost::system::error_code& ec) {
+		size_t numread = 0;
+		boost::asio::deadline_timer timeout(io_srv);
+		while (numread < len) {
+			size_t async_numread = 0;
+			//std::cout << "i";
+
+			timeout.expires_from_now(boost::posix_time::milliseconds(100));
+			timeout.async_wait(boost::bind(&KSerialPort::timeout_callback, this, boost::ref(mport), boost::asio::placeholders::error));
+			boost::asio::async_read(mport, boost::asio::buffer(&buffers[numread], len-numread), boost::asio::transfer_at_least(len-numread), boost::bind(&KSerialPort::async_read_callback, this, boost::ref(timeout), boost::ref(async_numread), _1, _2));
+			io_srv.run();
+			//std::cout << "/o ";
+			if (async_numread == 0) {
+				break;
+			}
+			numread += async_numread;
+		}
+		io_srv.reset();
+		//std::cout << "\nread: " << std::string(buffers, numread) << std::endl;
+		return numread;
+	}
+	int readLine(uint8_t *buffers, int len, boost::system::error_code& ec) {
+		size_t numread = 0;
+		boost::asio::deadline_timer timeout(io_srv);
+		while (numread < len) {
+			size_t async_numread = 0;
+
+			timeout.expires_from_now(boost::posix_time::milliseconds(100));
+			timeout.async_wait(boost::bind(&KSerialPort::timeout_callback, this, boost::ref(mport), boost::asio::placeholders::error));
+			boost::asio::async_read(mport, boost::asio::buffer(&buffers[numread], len-numread), boost::asio::transfer_at_least(1), boost::bind(&KSerialPort::async_read_callback, this, boost::ref(timeout), boost::ref(async_numread), _1, _2));
+			io_srv.run();
+			if (async_numread == 0) {
+				break;
+			}
+			numread += async_numread;
+			if (buffers[numread-1] == '\n') {
+				break;
+			}
+		}
+		io_srv.reset();
+		return numread;
+	}
+	int write_some(const std::vector<uint8_t> &buffers, boost::system::error_code& ec) {
+		return mport.write_some(boost::asio::buffer(buffers, buffers.size()), ec);
+	}
+	int write_some(const uint8_t *buffers, int len, boost::system::error_code& ec) {
+		return mport.write_some(boost::asio::buffer(buffers, len), ec);
+	}
+
+private:
+	void async_read_callback(boost::asio::deadline_timer &timeout, std::size_t &out_numread, const boost::system::error_code& error, std::size_t bytes_transferred) {
+		timeout.cancel();
+		if (!error) {
+			out_numread = bytes_transferred;
+		}
+	}
+
+private:
+	boost::asio::io_service io_srv;
+	boost::asio::serial_port mport;
+};
+
+#endif // KSERIALPORT_H_
